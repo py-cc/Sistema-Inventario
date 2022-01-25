37680d1099c49bd2a10572e449a991d13d83d24c
Jonathan-Hdzu <jonathanurbina088@gmail.com>
2022-01-25 00:14:47 -0600

    Agregado patrón factory

---
+++ my-app/src/test/java/com/team8/project/Inventario/ProductoTest.java
@@ -0,0 +1,13 @@
+package com.team8.project.Inventario;
+
+import junit.framework.TestCase;
+
+public class ProductoTest extends TestCase {
+    Producto product = new Producto(12, "hola", "testt");
+/*
+    public void registerTest(){
+        assertEquals("hola", product.registrarProducto(););
+    }
+*/
+
+}
